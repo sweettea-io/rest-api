f238ce013fbaa03f93224358f0620fe04a0c652f
Ben Whittle <benwhittle31@gmail.com>
2018-05-27 15:10:57 -0700

    emptied main.go

--- main.go
+++ main.go
@@ -1,15 +1,3 @@
 package main
 
-import (
-  "github.com/sweettea/rest-api/pkg/models"
-  "github.com/sweettea/rest-api/pkg/database"
-)
-
-func main() {
-  db := database.Connection()
-
-  // Create new user
-  team := &models.Team{Name: "Gabs Team"}
-
-  db.Create(&team)
-}
+func main() {}
