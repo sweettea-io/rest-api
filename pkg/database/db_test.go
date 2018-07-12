c519eccc9e1681b8c7bb2bc0a0607e8b94da65f3
Ben Whittle <benwhittle31@gmail.com>
2018-07-12 11:13:02 -0700

    switch db_test to 1-test-func-per-func style

--- pkg/database/db_test.go
+++ pkg/database/db_test.go
@@ -2,13 +2,13 @@ package database
 
 import "testing"
 
-// Connection should fail if it can't connect to the database url.
-func TestConnectionPanicsIfCantConnect(t *testing.T) {
+func TestConnection(t *testing.T) {
   defer func() {
     if r:= recover(); r == nil {
-      t.Errorf("Connection did not panic for bad DB url.")
+      t.Errorf("connection did not panic for bad DB url")
     }
   }()
 
+  // Connection should fail if it can't connect to the database url.
   Connection("bad-db-url")
 }
