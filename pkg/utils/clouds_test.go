ea9b8d7d27654f5b73363de505afd4e5a5e9f8c1
Ben Whittle <benwhittle31@gmail.com>
2018-07-12 11:13:47 -0700

    add clouds_test

---
+++ pkg/utils/clouds_test.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestIsValidCloud(t *testing.T) {
+  testCases := []struct {
+    name string
+    cloud string
+    expected bool
+  }{
+    {
+      name: "invalid cloud",
+      cloud: "asdasdfasdf",
+      expected: false,
+    },
+    {
+      name: "valid cloud",
+      cloud: "aws",
+      expected: true,
+    },
+  }
+
+  for _, tc := range testCases {
+    result := IsValidCloud(tc.cloud)
+    assert.Equal(t, tc.expected, result, tc.name)
+  }
+}
