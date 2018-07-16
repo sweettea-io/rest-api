821bf92b4b328278160a09b3c575b63f11d766ae
Ben Whittle <benwhittle31@gmail.com>
2018-07-16 17:00:54 -0400

    remove verbose test output; reset primary key sequences to 1 inside ClearTables

--- pkg/test_utils/db.go
+++ pkg/test_utils/db.go
@@ -20,6 +20,23 @@ func ClearTables(db *gorm.DB, originalLogMode bool) {
   db.Unscoped().Delete(&models.Commit{})
   db.Unscoped().Delete(&models.Deploy{})
 
+  tableNames := []string{
+    "users",
+    "sessions",
+    "companies",
+    "clusters",
+    "projects",
+    "datasets",
+    "envs",
+    "commits",
+    "deploys",
+  }
+
+  for _, tableName := range tableNames {
+    // Reset all primary key sequences to 1.
+    db.Exec("ALTER SEQUENCE ? RESTART WITH 1;", tableName + "_id_seq")
+  }
+
   // Revert DB log mode to its original value.
   db.LogMode(originalLogMode)
 }
