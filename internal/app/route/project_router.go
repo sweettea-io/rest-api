43daae99b4e1ef1320a35753ab66a76977a8e060
Ben Whittle <benwhittle31@gmail.com>
2018-07-18 21:47:29 -0700

    add service folder with usersvc pkg

--- internal/app/route/project_router.go
+++ internal/app/route/project_router.go
@@ -1,6 +1,11 @@
 package route
 
-import "net/http"
+import (
+  "net/http"
+  "github.com/sweettea-io/rest-api/internal/pkg/service/usersvc"
+  "github.com/sweettea-io/rest-api/internal/app/respond"
+  "github.com/sweettea-io/rest-api/internal/app/errmsg"
+)
 
 // ----------- ROUTER SETUP ------------
 
@@ -19,17 +24,45 @@ func InitProjectRouter() {
 }
 
 func CreateProjectHandler(w http.ResponseWriter, req *http.Request) {
+  // Find user from request session.
+  user, err := usersvc.FromRequest(req)
 
+  if err != nil {
+    respond.Error(w, errmsg.UserNotFound())
+  }
+
+  // Do user shit
 }
 
 func ReadProjectHandler(w http.ResponseWriter, req *http.Request) {
+  // Find user from request session.
+  user, err := usersvc.FromRequest(req)
 
+  if err != nil {
+    respond.Error(w, errmsg.UserNotFound())
+  }
+
+  // Do user shit
 }
 
 func UpdateProjectHandler(w http.ResponseWriter, req *http.Request) {
+  // Find user from request session.
+  user, err := usersvc.FromRequest(req)
 
+  if err != nil {
+    respond.Error(w, errmsg.UserNotFound())
+  }
+
+  // Do user shit
 }
 
 func DeleteProjectHandler(w http.ResponseWriter, req *http.Request) {
+  // Find user from request session.
+  user, err := usersvc.FromRequest(req)
 
+  if err != nil {
+    respond.Error(w, errmsg.UserNotFound())
+  }
+
+  // Do user shit
 }
---
+++ internal/pkg/service/usersvc/query.go
@@ -0,0 +1,29 @@
+package usersvc
+
+import (
+  "errors"
+  "net/http"
+  "github.com/sweettea-io/rest-api/internal/app"
+  "github.com/sweettea-io/rest-api/internal/pkg/model"
+)
+
+func FromRequest(req *http.Request) (*model.User, error) {
+  token := req.Header.Get(app.Config.AuthHeaderName)
+  e := errors.New(http.StatusText(http.StatusUnauthorized))
+
+  // Return error if auth header token not found.
+  if token == "" {
+    return nil, e
+  }
+
+  // Find session by token.
+  var session model.Session
+  result := app.DB.Preload("User").Where(&model.Session{Token: token}).First(&session)
+
+  // Return error if session doesn't exist or user doesn't exist through session.
+  if result.RecordNotFound() || session.User.ID == 0 {
+    return nil, e
+  }
+
+  return &session.User, nil
+}
