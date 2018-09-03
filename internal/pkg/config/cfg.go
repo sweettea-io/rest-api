a76cea5ba510debc7eba3b0378ae640647ff9ff1
Ben Whittle <benwhittle31@gmail.com>
2018-09-03 12:43:51 -0700

    update bp support to include python-websocket-api buildpack; update env defaults.json

--- internal/pkg/config/cfg.go
+++ internal/pkg/config/cfg.go
@@ -10,46 +10,49 @@ import (
 
 // Config represents app config populated from environment variables.
 type Config struct {
-  APIVersion                        string `env:"API_VERSION,required"`
-  AuthHeaderName                    string `env:"AUTH_HEADER_NAME,required"`
-  AWSAccessKeyId                    string `env:"AWS_ACCESS_KEY_ID,required,ignore_on_envs=test|local"`
-  AWSRegionName                     string `env:"AWS_REGION_NAME,required,ignore_on_envs=test|local"`
-  AWSSecretAccessKey                string `env:"AWS_SECRET_ACCESS_KEY,required,ignore_on_envs=test|local"`
-  BuildClusterName                  string `env:"BUILD_CLUSTER_NAME,required,ignore_on_envs=test"`
-  BuildClusterState                 string `env:"BUILD_CLUSTER_STATE,required,ignore_on_envs=test|local"`
-  CloudProvider                     string `env:"CLOUD_PROVIDER,required,ignore_on_envs=test|local"`
-  DatabaseUrl                       string `env:"DATABASE_URL,required"`
-  Debug                             bool   `env:"DEBUG,required"`
-  DNSService                        string `env:"DNS_SERVICE,required,ignore_on_envs=test|local"`
-  DeployReplicasCount               int32  `env:"DEPLOY_REPLICAS_COUNT,required,ignore_on_envs=test"`
-  DockerRegistryOrg                 string `env:"DOCKER_REGISTRY_ORG,required,ignore_on_envs=test"`
-  DockerRegistryUsername            string `env:"DOCKER_REGISTRY_USERNAME,required,ignore_on_envs=test"`
-  DockerRegistryPassword            string `env:"DOCKER_REGISTRY_PASSWORD,required,ignore_on_envs=test"`
-  Domain                            string `env:"DOMAIN,required,ignore_on_envs=test"`
-  Env                               string `env:"ENV,required"`
-  GitHubAccessToken                 string `env:"GITHUB_ACCESS_TOKEN"`
-  HostedZoneId                      string `env:"HOSTED_ZONE_ID,ignore_on_envs=test|local"`
-  JobQueueNsp                       string `env:"JOB_QUEUE_NSP,required"`
-  KubeConfig                        string `env:"KUBECONFIG,required"`
-  ModelStorageUrl                   string `env:"MODEL_STORAGE_URL,required,ignore_on_envs=test"`
-  PythonJsonApiBuildpackAccessToken string `env:"PYTHON_JSON_API_BUILDPACK_ACCESS_TOKEN"`
-  PythonJsonApiBuildpackSha         string `env:"PYTHON_JSON_API_BUILDPACK_SHA,required"`
-  PythonJsonApiBuildpackUrl         string `env:"PYTHON_JSON_API_BUILDPACK_URL,required"`
-  PythonTrainBuildpackAccessToken   string `env:"PYTHON_TRAIN_BUILDPACK_ACCESS_TOKEN"`
-  PythonTrainBuildpackSha           string `env:"PYTHON_TRAIN_BUILDPACK_SHA,required"`
-  PythonTrainBuildpackUrl           string `env:"PYTHON_TRAIN_BUILDPACK_URL,required"`
-  RedisPoolMaxActive                int    `env:"REDIS_POOL_MAX_ACTIVE,required"`
-  RedisPoolMaxIdle                  int    `env:"REDIS_POOL_MAX_IDLE,required"`
-  RedisPoolWait                     bool   `env:"REDIS_POOL_WAIT,required"`
-  RedisAddress                      string `env:"REDIS_ADDRESS,required"`
-  RedisPassword                     string `env:"REDIS_PASSWORD"`
-  RestApiToken                      string `env:"REST_API_TOKEN,required"`
-  ServerPort                        int    `env:"SERVER_PORT,required"`
-  TrainClusterName                  string `env:"TRAIN_CLUSTER_NAME"`
-  TrainClusterState                 string `env:"TRAIN_CLUSTER_STATE"`
-  UserCreationHash                  string `env:"USER_CREATION_HASH"`
-  WildcardSSLCertArn                string `env:"WILDCARD_SSL_CERT_ARN,required,ignore_on_envs=test|local"`
-  WorkerCount                       uint   `env:"WORKER_COUNT,required,ignore_on_envs=test"`
+  APIVersion                             string `env:"API_VERSION,required"`
+  AuthHeaderName                         string `env:"AUTH_HEADER_NAME,required"`
+  AWSAccessKeyId                         string `env:"AWS_ACCESS_KEY_ID,required,ignore_on_envs=test|local"`
+  AWSRegionName                          string `env:"AWS_REGION_NAME,required,ignore_on_envs=test|local"`
+  AWSSecretAccessKey                     string `env:"AWS_SECRET_ACCESS_KEY,required,ignore_on_envs=test|local"`
+  BuildClusterName                       string `env:"BUILD_CLUSTER_NAME,required,ignore_on_envs=test"`
+  BuildClusterState                      string `env:"BUILD_CLUSTER_STATE,required,ignore_on_envs=test|local"`
+  CloudProvider                          string `env:"CLOUD_PROVIDER,required,ignore_on_envs=test|local"`
+  DatabaseUrl                            string `env:"DATABASE_URL,required"`
+  Debug                                  bool   `env:"DEBUG,required"`
+  DNSService                             string `env:"DNS_SERVICE,required,ignore_on_envs=test|local"`
+  DeployReplicasCount                    int32  `env:"DEPLOY_REPLICAS_COUNT,required,ignore_on_envs=test"`
+  DockerRegistryOrg                      string `env:"DOCKER_REGISTRY_ORG,required,ignore_on_envs=test"`
+  DockerRegistryUsername                 string `env:"DOCKER_REGISTRY_USERNAME,required,ignore_on_envs=test"`
+  DockerRegistryPassword                 string `env:"DOCKER_REGISTRY_PASSWORD,required,ignore_on_envs=test"`
+  Domain                                 string `env:"DOMAIN,required,ignore_on_envs=test"`
+  Env                                    string `env:"ENV,required"`
+  GitHubAccessToken                      string `env:"GITHUB_ACCESS_TOKEN"`
+  HostedZoneId                           string `env:"HOSTED_ZONE_ID,ignore_on_envs=test|local"`
+  JobQueueNsp                            string `env:"JOB_QUEUE_NSP,required"`
+  KubeConfig                             string `env:"KUBECONFIG,required"`
+  ModelStorageUrl                        string `env:"MODEL_STORAGE_URL,required,ignore_on_envs=test"`
+  PythonJsonApiBuildpackAccessToken      string `env:"PYTHON_JSON_API_BUILDPACK_ACCESS_TOKEN"`
+  PythonJsonApiBuildpackSha              string `env:"PYTHON_JSON_API_BUILDPACK_SHA,required"`
+  PythonJsonApiBuildpackUrl              string `env:"PYTHON_JSON_API_BUILDPACK_URL,required"`
+  PythonTrainBuildpackAccessToken        string `env:"PYTHON_TRAIN_BUILDPACK_ACCESS_TOKEN"`
+  PythonTrainBuildpackSha                string `env:"PYTHON_TRAIN_BUILDPACK_SHA,required"`
+  PythonTrainBuildpackUrl                string `env:"PYTHON_TRAIN_BUILDPACK_URL,required"`
+  PythonWebsocketApiBuildpackAccessToken string `env:"PYTHON_WEBSOCKET_API_BUILDPACK_ACCESS_TOKEN"`
+  PythonWebsocketApiBuildpackSha         string `env:"PYTHON_WEBSOCKET_API_BUILDPACK_SHA,required"`
+  PythonWebsocketApiBuildpackUrl         string `env:"PYTHON_WEBSOCKET_API_BUILDPACK_URL,required"`
+  RedisPoolMaxActive                     int    `env:"REDIS_POOL_MAX_ACTIVE,required"`
+  RedisPoolMaxIdle                       int    `env:"REDIS_POOL_MAX_IDLE,required"`
+  RedisPoolWait                          bool   `env:"REDIS_POOL_WAIT,required"`
+  RedisAddress                           string `env:"REDIS_ADDRESS,required"`
+  RedisPassword                          string `env:"REDIS_PASSWORD"`
+  RestApiToken                           string `env:"REST_API_TOKEN,required"`
+  ServerPort                             int    `env:"SERVER_PORT,required"`
+  TrainClusterName                       string `env:"TRAIN_CLUSTER_NAME"`
+  TrainClusterState                      string `env:"TRAIN_CLUSTER_STATE"`
+  UserCreationHash                       string `env:"USER_CREATION_HASH"`
+  WildcardSSLCertArn                     string `env:"WILDCARD_SSL_CERT_ARN,required,ignore_on_envs=test|local"`
+  WorkerCount                            uint   `env:"WORKER_COUNT,required,ignore_on_envs=test"`
 }
 
 // BaseRoute returns the base route for the server app's API.
@@ -97,6 +100,9 @@ func (cfg *Config) BuildpackEnvs() map[string]string {
     "PYTHON_TRAIN_BUILDPACK_ACCESS_TOKEN": cfg.PythonTrainBuildpackAccessToken,
     "PYTHON_TRAIN_BUILDPACK_SHA": cfg.PythonTrainBuildpackSha,
     "PYTHON_TRAIN_BUILDPACK_URL": cfg.PythonTrainBuildpackUrl,
+    "PYTHON_WEBSOCKET_API_BUILDPACK_ACCESS_TOKEN": cfg.PythonWebsocketApiBuildpackAccessToken,
+    "PYTHON_WEBSOCKET_API_BUILDPACK_SHA": cfg.PythonWebsocketApiBuildpackSha,
+    "PYTHON_WEBSOCKET_API_BUILDPACK_URL": cfg.PythonWebsocketApiBuildpackUrl,
   }
 }
 
